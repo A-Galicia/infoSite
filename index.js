db089300484ffb4e01774fe7635d05c3af9ec3f7
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-06-26 20:11:01 -0700

    reworked with Express.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const http = require('node:http');
-const url = require('node:url');
+// With Express
+const path = require('node:path');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/html', '/index.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/html', '/about.html'));
+});
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, '/html', '/contact-me.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
+
+// just with Node.js
+
+/* const http = require('node:http');
+const path = require('node:path');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
+  writeToPage(req, res);
   if (req.url == '/') {
     fs.readFile('./html/index.html', (err, data) => {
       if (err === null) {
@@ -12,7 +36,7 @@ const server = http.createServer((req, res) => {
       }
     });
   } else {
-    fs.readFile('./html/' + req.url, (err, data) => {
+    fs.readFile(path.join(__dirname, '/html', req.url), (err, data) => {
       if (err === null) {
         writeToPage(data, res);
       } else {
@@ -22,7 +46,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8080);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT);
 
 // Functions //////////////////////////////////////////////////////
 function writeToPage(data, res) {
@@ -32,9 +57,10 @@ function writeToPage(data, res) {
 }
 
 function show404(res) {
-  fs.readFile('./html/404.html', (err, data) => {
+  fs.readFile(path.join(__dirname, '/html', '/404.html'), (err, data) => {
     if (err === null) {
       writeToPage(data, res);
     }
   });
 }
+ */
